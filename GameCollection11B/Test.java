29fa43b1208505568db1a46ca2237bce65e0aca9
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2022-05-30 14:04:11 +0200

    Create Test.java

---
+++ GameCollection11B/Test.java
@@ -0,0 +1,27 @@
+public class BallGame extends Game {
+
+   private Circle ball;
+   
+   public BallGame(View returnTo) {
+      super(returnTo);
+   }
+
+   public void build() {
+      super.build();
+      add(overlay);
+      ball = new Circle(100, 100, 20);
+      ball.setFillColor(Color.blueviolet);
+      add(ball);
+   }
+
+   public void act() {
+      ball.move(-2, -2);
+      if(ball.isOutsideView()) {
+         destroy();
+         println("hilfe");
+         // return_(true, true);
+         stopActing();
+      }
+   }
+
+}
