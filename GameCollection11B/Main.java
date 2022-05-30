a501f8782bcd5ed04aa8eec1acf418973c8e0520
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2022-05-30 14:03:10 +0200

    Create Main.java

---
+++ GameCollection11B/Main.java
@@ -0,0 +1,6 @@
+// SystemTools.setSpeed(-1);
+Rectangle background = new Rectangle(0, 0, 800, 600);
+background.setFillColor(Theming.background);
+MainMenu mainMenu = new MainMenu();
+mainMenu.build();
+mainMenu.show(true);
