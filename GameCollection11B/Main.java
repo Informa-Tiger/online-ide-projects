ec07cf5fcd84a2c9e57c102c1d6a1329df378392
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2022-06-20 18:57:57 +0200

    Update Main.java

--- GameCollection11B/Main.java
+++ GameCollection11B/Main.java
@@ -1,6 +1,13 @@
 // SystemTools.setSpeed(-1);
 Rectangle background = new Rectangle(0, 0, 800, 600);
 background.setFillColor(Theming.background);
+
+HiddenGroup hidden = new HiddenGroup();
+hidden.setVisible(false);
+hidden.getWorld().setDefaultGroup(hidden);
+
 MainMenu mainMenu = new MainMenu();
 mainMenu.build();
 mainMenu.show(true);
+
+class HiddenGroup extends Group { }
