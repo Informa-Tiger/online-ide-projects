275d1cad4220c11ed093af7635be78ff29a91cbc
Informa-Tiger <67505689+Informa-Tiger@users.noreply.github.com>
2022-07-01 19:09:40 +0200

    Create pr_55_Group_copy.java

---
+++ embedded_examples/pr_55_Group_copy.java
@@ -0,0 +1,8 @@
+Rectangle r = new Rectangle(0, 0, 100, 100);
+Group g = new Group(r);
+Group defaultGroup = new Group();
+defaultGroup.getWorld().setDefaultGroup(defaultGroup);
+Group copy = g.copy();
+g.destroy();
+copy.destroy();
+println(defaultGroup.get(0).getCenterX());
