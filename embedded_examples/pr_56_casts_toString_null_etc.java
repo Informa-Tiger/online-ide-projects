ef0995a0239870a7cec057b59e057cb849654b3d
InformaTiger <informatiger@pemoma.de>
2022-07-02 18:43:15 +0200

    updated pr_56_casts_toString_null_etc.java

--- embedded_examples/pr_56_casts_toString_null_etc.java
+++ embedded_examples/pr_56_casts_toString_null_etc.java
@@ -1,6 +1,9 @@
 println(3 / 2d);                                         // Expected: 1.5
 println(3.);                                             // Expected: 3.0
+println((long)(Integer) 1);                              // Expected: 1
 println(null);                                           // Expected: null
+println((Rectangle) null);                               // Expected: null
 println((String)null + (String)null);                    // Expected: nullnull
-println(null == new Circle(0, 0, 0).getParentGroup());   // Expected: true
-double a = (Double) null;                                // Expected an error
+println(null == (Circle) null);                          // Expected: true
+// double a = (Double) null;                             // Expected an error
+// String s = new Rectangle(0,0,0,0); println(s);        // Expected an error
